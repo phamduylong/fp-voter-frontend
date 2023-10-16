8347b052fe1719c4efb923b34ee7ba061477a7d8
longph <longphamduy2002@gmail.com>
2023-10-16 20:49:27 +0300

    Front-end restructure

--- web-intf/backend/models/JWT.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const JWTSchema = new Schema({
-    token: {type: String, required: true},
-    expiryTime: { type: Date, required: true}
-  });
-  
-
-module.exports = mongoose.model("JWT", JWTSchema);
