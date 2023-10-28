33c7ee5a15e6cdf897dce1511d3b8fb64546b329
Long Pham <69810988+phamduylong@users.noreply.github.com>
2023-10-28 14:28:19 +0300

    Feature/FPV-30  (#2)
    * FPV-30 WIP
    
    * FPV-30 candidates page layout design
    
    * FPV-30 error handing template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+    // for ease of testing, should be deleted as we implement main page
+    onMount(async () => {
+        await goto('/login');
+    })
+</script>
