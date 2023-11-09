cd6be2831e703fa98f3ed1adb5fa67e59ac82a1f
longph <longphamduy2002@gmail.com>
2023-11-10 01:45:25 +0200

    FPV-48 TnC page changes

--- src/routes/(app)/terms-conditions/+page.js
+++ src/routes/(app)/terms-conditions/+page.js
@@ -1,52 +1,73 @@
-import { error } from '@sveltejs/kit';
-import { browser } from '$app/environment';
-
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
     return {
         title: `Terms And Conditions`,
-        content: `
-                    <b>1. Acceptance of Terms</b><br>
-                    By accessing or using the PiNKK fingerprint voting system ("the System"), you agree to comply with and be bound by these Terms and Conditions.
-                    <br><br>
-
-                    <b>2. Use of the System</b><br>
-                    a. You must use the System in accordance with all applicable laws and regulations.<br>
-                    b. Unauthorized use of the System is strictly prohibited.
-                    <br><br>
-
-                    <b>3. Privacy and Data Security</b><br>
-                    a. We are committed to protecting your privacy. All personal and biometric data collected during the voting process will be handled in accordance with our Privacy Policy. <br>
-                    b. The System employs robust security measures to protect against unauthorized access and data breaches.
-                    <br><br>
-
-                    <b>4. Voter Responsibilities</b><br>
-                    a. Voters are responsible for maintaining the confidentiality of their authentication information. <br>
-                    b. Any misuse of the System, including attempts to tamper with or manipulate the voting process, is strictly prohibited.
-                    <br><br>
-
-                    <b>5. Intellectual Property</b><br>
-                    a. All intellectual property rights related to the System, including but not limited to software, trademarks, and design, are owned by PiNKK.
-                    <br><br>
-
-                    <b>6. Limitation of Liability</b><br>
-                    a. PiNKK shall not be liable for any indirect, incidental, or consequential damages arising out of the use or inability to use the System.
-                    <br><br>
-
-                    <b>7. Modifications</b><br>
-                    a. PiNKK reserves the right to modify, suspend, or discontinue the System at any time without notice.
-                    <br><br>
-
-                    <b>8. Governing Law</b><br>
-                    a. These Terms and Conditions shall be governed by and construed in accordance with the laws of European Union.
-                    <br><br>
-
-                    <b>9. Termination</b><br>
-                    a. PiNKK reserves the right to terminate your access to the System for any reason without prior notice.
-                    <br><br>
-
-                    <b>10. Contact Information</b><br>
-                    For any questions or concerns regarding these Terms and Conditions, please contact us at registry.vnk@gov.fi.
-                    <br><br>`
+        content: [
+            {
+                subtitle: `1. Acceptance of Terms`,
+                subcontent: [
+                                `By accessing or using the PiNKK fingerprint voting system ("the System"), 
+                                you agree to comply with and be bound by these Terms and Conditions.`
+                            ]
+            },
+            {
+                subtitle: `2. Use of the System`,
+                subcontent: [
+                                `You must use the System in accordance with all applicable laws and regulations.`,
+                                `Unauthorized use of the System is strictly prohibited.`
+                            ]
+            },
+            {
+                subtitle: `3. Privacy and Data Security`,
+                subcontent: [
+                                `We are committed to protecting your privacy. All personal and biometric data collected during the voting process 
+                                will be handled in accordance with our Privacy Policy.`,
+                                `The System employs robust security measures to protect against unauthorized access and data breaches.`
+                            ]
+            },
+            {
+                subtitle: `4. Voter Responsibilities`,
+                subcontent: [
+                                `Voters are responsible for maintaining the confidentiality of their authentication information.`,
+                                `Any misuse of the System, including attempts to tamper with or manipulate the voting process, is strictly prohibited.`
+                            ]
+            },
+            {
+                subtitle: `5. Intellectual Property`,
+                subcontent: [
+                                `All intellectual property rights related to the System, including but not limited to software, trademarks, and design, are owned by PiNKK.`
+                            ]
+            },
+            {
+                subtitle: `6. Limitation of Liability`,
+                subcontent: [
+                                `PiNKK shall not be liable for any indirect, incidental, or consequential damages arising out of the use or inability to use the System.`
+                            ]
+            },
+            {
+                subtitle: `7. Modifications`,
+                subcontent: [
+                                `PiNKK reserves the right to modify, suspend, or discontinue the System at any time without notice.`
+                            ]
+            },
+            {
+                subtitle: `8. Governing Law`,
+                subcontent: [
+                                `These Terms and Conditions shall be governed by and construed in accordance with the laws of European Union.`
+                            ]
+            },
+            {
+                subtitle: `9. Termination`,
+                subcontent: [
+                                `PiNKK reserves the right to terminate your access to the System for any reason without prior notice.`
+                            ]
+            },
+            {
+                subtitle: `10. Contact Information`,
+                subcontent: [
+                                `For any questions or concerns regarding these Terms and Conditions, please contact us at registry.vnk@gov.fi.`
+                            ]
+            },
+        ]
     };
 }
--- src/routes/(app)/terms-conditions/+page.svelte
+++ src/routes/(app)/terms-conditions/+page.svelte
@@ -2,9 +2,19 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
-
-<div class="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-2 lg:p-10 w-3/4">
-<h1 style="font-size:20px ;"><b>{data.title}</b></h1>
-<br><br>
-<div>{@html data.content}</div>
+<br>
+<h1 class="flex justify-center text-xl"><b>{data.title}</b></h1>
+<div class="w-full flex justify-center text-justify p-4 lg:p-10">
+<div>
+	{#each data.content as paragraph}
+		<b>{paragraph.subtitle}</b>
+		<div class="px-4">
+			<br>
+			{#each paragraph.subcontent as subcontent}
+				<li>{subcontent}</li>
+			{/each}
+		</div>
+		<br>
+	{/each}
+</div>
 </div>
