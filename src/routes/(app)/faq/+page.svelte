438f58e0865a18ec90f71ab8b8232ec300c80777
longph <longphamduy2002@gmail.com>
2023-11-09 23:52:41 +0200

    FPV-48 minor details for FAQ

--- src/routes/(app)/faq/+page.svelte
+++ src/routes/(app)/faq/+page.svelte
@@ -7,9 +7,9 @@
 <h1 class="h1 text-xl flex justify-center p-4 lg:p-0 lg:pt-8"><b>{data.title}</b></h1>
 <div class="lg:p-10 lg:w-3/4 lg:relative lg:top-1/2 lg:left-1/2 lg: lg:-translate-x-1/2 lg:-translate-y-1/2">
   <div>
-    <Accordion>
+    <Accordion class="mb-10 px-10">
       {#each data.content as pair}
-        <AccordionItem>
+        <AccordionItem class="card card-hover p-2">
 		  <svelte:fragment slot="lead"><i class="fa fa-info-circle"></i></svelte:fragment>
           <svelte:fragment slot="summary"><b>{pair.q}</b></svelte:fragment>
           <svelte:fragment slot="content"
