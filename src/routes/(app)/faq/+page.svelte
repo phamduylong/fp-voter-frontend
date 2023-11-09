342c3e232cf063d9088b0f70a9e82a8bc9d804e1
longph <longphamduy2002@gmail.com>
2023-11-09 23:28:02 +0200

    FPV-48 FAQ page enhancement

--- src/routes/(app)/faq/+page.svelte
+++ src/routes/(app)/faq/+page.svelte
@@ -1,10 +1,22 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	export let data;
+  /** @type {import('./$types').PageData} */
+  export let data;
+  import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
 </script>
 
-<div class="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  p-2 lg:p-10 w-3/4">
-<h1 style="font-size:20px ;"><b>{data.title}</b></h1>
-<br><br>
-<div>{@html data.content}</div>
+<h1 class="h1 text-xl flex justify-center p-4 lg:p-0 lg:pt-8"><b>{data.title}</b></h1>
+<div class="lg:p-10 lg:w-3/4 lg:relative lg:top-1/2 lg:left-1/2 lg: lg:-translate-x-1/2 lg:-translate-y-1/2">
+  <div>
+    <Accordion>
+      {#each data.content as pair}
+        <AccordionItem>
+		  <svelte:fragment slot="lead"><i class="fa fa-info-circle"></i></svelte:fragment>
+          <svelte:fragment slot="summary"><b>{pair.q}</b></svelte:fragment>
+          <svelte:fragment slot="content"
+            ><p class="p-2 text-justify">{pair.a}</p></svelte:fragment
+          >
+        </AccordionItem>
+      {/each}
+    </Accordion>
+  </div>
 </div>
