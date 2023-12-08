a0a84722e33a00465fcb3146cd530fb24c28b7ff
kendrick <tkogt12@gmail.com>
2023-12-08 13:38:26 +0200

    Hot fix result page 2

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
 <script>
 	import { page } from '$app/stores';
     import { alertState } from "$lib/alertStore.js";
-    alertState.show($page.error.message.body.message, "error");
+    alertState.show($page.error?.message?.body?.message ?? "Unexpected error occurred.", "error");
 </script>
